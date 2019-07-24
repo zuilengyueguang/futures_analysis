871ea68cf58ce8acc807ae61e3b4fd315e44556d
lipeng <973202782@qq.com>
2019-07-24 21:53:11 +0800

    1. 添加聚宽终端的投资研究内容，后续会每日更新一次 2. 添加vnpy的数据表结构记录

--- analysis/engine.py
+++ analysis/engine.py
@@ -4,10 +4,34 @@ from pandas import Series, DataFrame
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import statsmodels.tsa.stattools as st
-import re
 import datetime
-from jqdata import jy
-from numpy import nan
+from matplotlib.ticker import FuncFormatter
+
+
+def plotData(data, title='', signal=False):
+    def format_date(x, pos=None):
+        if x < 0 or x > len(date_tickers) - 1:
+            return ''
+        return date_tickers[int(x)]
+
+    plt.figure(figsize=(16, 5.2))
+    plt.rcParams['font.sans-serif'] = ['SimHei']  # 解决中文显示乱码的问题
+    date_tickers = data.index.tolist()
+    newData = data.copy()
+    newData.index = range(newData.size)
+    ax = plt.subplot()
+    ax.xaxis.set_major_formatter(FuncFormatter(format_date))
+    newData.plot(ax=ax, title=title)
+    if signal:
+        mu = np.mean(data)
+        std = np.std(data)
+        plt.axhline(y=mu, color='black')
+        plt.axhline(y=mu + 0.2 * std, color='blue')
+        plt.axhline(y=mu - 0.2 * std, color='blue')
+        plt.axhline(y=mu + 1.5 * std, color='green')
+        plt.axhline(y=mu - 1.5 * std, color='green')
+        plt.axhline(y=mu + 2.5 * std, color='red')
+        plt.axhline(y=mu - 2.5 * std, color='red')
 
 
 # 数据引擎
@@ -26,31 +50,34 @@ class DataEngine():
         df1.drop(columns='time', inplace=True)
         return df1
 
-    def getData(self, symbol, start_date, end_date, sec):
-        data = self.getTickData(symbol, start_date=start_date, end_date=end_date)
-        return data
-
-    # 取前一周的Tick数据
-    def getPreWeekTickData(self, symbol):
-        e = datetime.datetime.now()
-        end_date = e.strftime('%Y-%m-%d')
-        s = e - datetime.timedelta(days=7)
-        start_date = s.strftime('%Y-%m-%d')
-        return self.getTickData(symbol, start_date=start_date, end_date=end_date)
-
-    # 获取某个合约的前一周的秒级数据
-
-    # 由tick数据转换为秒级数据
-    def getPreWeekSecData(self, symbol, freq=1):
-        df1 = self.getPreWeekTickData(symbol)
-        df1 = df1.current.resample('%dS' % freq).agg(['last', 'first', 'max', 'min']).dropna()
-        # 筛选固定交易时间数据，只做日内，并且不做夜盘
-        # 这里的数据筛选是特定的针对性数据
-        d1 = df1.loc[datetime.time(9, 0, 0):datetime.time(10, 15, 0), :]
-        d2 = df1.loc[datetime.time(10, 30, 0):datetime.time(11, 30, 0), :]
-        d3 = df1.loc[datetime.time(13, 30, 0):datetime.time(15, 0, 0), :]
-        # 连接函数
-        return pd.concat([d1, d2, d3])
+    def getData(self, symbol, start_date, end_date, sec='1S'):
+        if sec.endswith('S'):
+            tickData = self.getTickData(symbol, start_date=start_date, end_date=end_date)
+            changeData = tickData.loc[:, ['current', 'volume', 'money']].resample(sec).agg(
+                {'current': ['last', 'first', 'max', 'min'], 'volume': 'last', 'money': 'last'})
+            data = changeData.current
+            data = data.rename(columns={'first': 'open', 'last': 'close', 'max': 'high', 'min': 'low'})
+            data['volume'] = changeData.volume['last'].diff().fillna(0)
+            data['money'] = changeData.money['last'].diff().fillna(0)
+            d1 = data.loc[datetime.time(9, 0, 0):datetime.time(10, 15, 0), :]
+            d2 = data.loc[datetime.time(10, 30, 0):datetime.time(11, 30, 0), :]
+            d3 = data.loc[datetime.time(13, 30, 0):datetime.time(15, 0, 0), :]
+            data = pd.concat([d1, d2, d3])
+            return data
+        elif sec.endswith('m'):
+            data = get_price('RM1909.XZCE', start_date=start_date, end_date=end_date, frequency=sec,
+                             fields=['open', 'close', 'high', 'low', 'volume', 'money'])
+            d1 = data.loc[datetime.time(9, 0, 0):datetime.time(10, 15, 0), :]
+            d2 = data.loc[datetime.time(10, 30, 0):datetime.time(11, 30, 0), :]
+            d3 = data.loc[datetime.time(13, 30, 0):datetime.time(15, 0, 0), :]
+            data = pd.concat([d1, d2, d3])
+            return data
+        elif sec.endswith('d'):
+            data = get_price('RM1909.XZCE', start_date=start_date, end_date=end_date, frequency=sec,
+                             fields=['open', 'close', 'high', 'low', 'volume', 'money'])
+            return data
+        else:
+            raise Exception("不支持的频率类型，请使用频率 1m分钟 1S秒 1d天")
 
     # 获取所有主力合约信息
     def getAllMainContract(self):
@@ -114,89 +141,101 @@ class StatisEngine():
         return priceY - alpha - beta * priceX
 
 
-# 一阶单整检验筛选
-def filter_integration_datas(ac):
+# 2. 一阶单整检验筛选
+def filter_integration_datas(ac, sec, ana_start, ana_end, trade_start, trade_end):
     de = DataEngine()
     se = StatisEngine()
-    print("1. 获取主力合约基本面数据")
     validData = {}
-    print("2. 开始一阶单整检验")
     for i in ac.index:
         # 获取经过处理的数据
-        print("%s 合约-------------1. 错误合约检验" % str(i))
-        if i not in error_stocks:
-            print("%s 合约-------------2. 获取秒级数据" % str(i))
-            data1 = de.getPreWeekSecData(i)
-            # 排除没有数据的合约
-            print("%s 合约-------------3. 数据量检验,数据量要在10000以上" % str(i))
-            if (data1.shape[0] >= 10000):
-                # 对秒级 频率数据进行一阶单整检验
-                print("%s 合约-------------4. 合约一阶单整检验" % str(i))
-                result = se.isIntegration(data1['last'])
-                if result == True:
-                    print("%s 合约-------------5. 合约一阶单整检验成功" % str(i))
+        print("%s 合约-------------进行一阶单整筛选" % str(i))
+        # print("%s 合约-------------1. 获取数据，数据进行筛选" % str(i))
+        tmp_data = de.getData(i, start_date=ana_start, end_date=trade_end, sec=sec)
+        # 排除没有数据的合约
+        # print("%s 合约-------------2. 最后一日成交量量要在10000手以上" % str(i))
+        # 获取最后一日的成交量
+        if tmp_data.shape[0]>10000:
+            volume = tmp_data[str(tmp_data.index[-1].date())].volume.sum()
+            if volume >= 10000:
+                # print("%s 合约-------------3. 获取分析期数据，并去重" % str(i))
+                close_ana_data = tmp_data.loc[ana_start:ana_end, :]['close'].dropna()
+                # print("%s 合约-------------4. 合约一阶单整检验" % str(i))
+                result = se.isIntegration(close_ana_data)
+                if result:
+                    # print("%s 合约-------------5. 合约一阶单整检验成功" % str(i))
                     # 用收盘价数据乘以最小交易单位作为要分析的数据
-                    last_data = data1['last']
                     cu = de.get_trade_units(str(i))
-                    print("%s 合约-------------6. cu:%s" % (str(i), str(cu)))
-                    validData[i] = (last_data * cu)
-    data = DataFrame(validData)
-    return data.dropna(how='all')
+                    close_data = tmp_data['close'].dropna()
+                    validData[i] = (close_data * cu)
+    data = DataFrame(validData).dropna(how='all')
+    ana_date = data.loc[ana_start:ana_end, :]
+    trade_date = data.loc[trade_start:trade_end, :]
+    return ana_date, trade_date
 
 
 # 协整效应筛选
-def filter_cointegration(ac, data):
-    l = data.columns.size
+def filter_cointegration(ac, ana_data, trade_data):
+    l = ana_data.columns.size
     se = StatisEngine()
-    pairs = []
+    pairs_list = []
+    pairs_ana_data = {}
+    pairs_trade_data = {}
     for i in range(l):
         for j in range(i + 1, l):
-            s1 = data.columns[i]
-            s2 = data.columns[j]
+            s1 = ana_data.columns[i]
+            s2 = ana_data.columns[j]
             # 前项和后项填充都用上，防止出现空值
-            t1 = data.loc[:, [s1, s2]].dropna(how='all').bfill().ffill()
+            t1 = ana_data.loc[:, [s1, s2]].dropna(how='all').bfill().ffill()
             d1 = t1[s1]
             d2 = t1[s2]
-            print("套利对：(%s,%s)开始协整检验" % (s1, s2))
+            print("套利对：(%s,%s)进行协整检验" % (s1, s2))
             (result, alpha, beta) = se.isCointegration(d1, d2)
             if result:
                 alpha = round(alpha + 0.1, 0)  # alpha值进行取整
                 beta = round(beta + 0.001, 2)  # beta值保留两位小数
                 s1_name = ac.loc[s1, :].display_name
                 s2_name = ac.loc[s2, :].display_name
-                print("套利对为：[%s,%s],合约名称:[%s,%s],alpha:%s,beta:%s"
-                      % (s1, s2, s1_name, s2_name, str(alpha), str(beta)))
-                pairs.append((s1, s2, s1_name, s2_name, alpha, beta))
-    pairs_d = DataFrame(pairs, columns=['s1', 's2', 's1_name', 's2_name', 'alpha', 'beta'])
-    pairs_d.index = pairs_d.s1 + '_' + pairs_d.s2
-    return pairs_d
+                pairs_list.append((s1, s2, s1_name, s2_name, alpha, beta))
+                pairs_ana_data[s1 + '_' + s2] = t1
+                pairs_trade_data[s1 + '_' + s2] = trade_data.loc[:, [s1, s2]].dropna(how='all').bfill().ffill()
+    pairs = DataFrame(pairs_list, columns=['s1', 's2', 's1_name', 's2_name', 'alpha', 'beta'])
+    pairs.index = pairs.s1 + '_' + pairs.s2
+    return pairs, pairs_ana_data, pairs_trade_data
 
 
-e = datetime.datetime.now()
-end_date = e.strftime('%Y-%m-%d')
-s = e - datetime.timedelta(days=7)
-start_date = s.strftime('%Y-%m-%d')
+def create_report(pairs, pairs_ana_data, pairs_trade_data):
+    print(pairs)
+    for i in pairs.index:
+        pair = pairs.loc[i, :]
+        ana_data = pairs_ana_data[i]
+        trade_data = pairs_trade_data[i]
+        ana_resid = ana_data[pair.s2] - pair.alpha - pair.beta * ana_data[pair.s1]
+        trade_resid = trade_data[pair.s2] - pair.alpha - pair.beta * trade_data[pair.s1]
+        plotData(ana_resid, title=('%s和%s分析期数据的价差图' % (pair.s1_name, pair.s2_name)), signal=True)
+        plotData(trade_resid, title=('%s和%s交易期数据的价差图' % (pair.s1_name, pair.s2_name)), signal=True)
 
 
-def Main(sec='1s', start_date=start_date, end_date=end_date):
-    # 改造为 可以传送频率 时间段 和 具体
-    # 获取主力合约基本面数据
+def Main(sec, ana_start, ana_end, trade_start, trade_end):
+    print("1. 获取合约信息")
     de = DataEngine()
     ac = de.getAllMainContract()
-    # 一阶单整筛选
-    data = filter_integration_datas(ac)
-    # 协整检验
-    pairs = filter_cointegration(ac, data)
+    print("2. 进行一阶单整筛选")
+    (ana_date, trade_data) = filter_integration_datas(ac, sec=sec, ana_start=ana_start,
+                                                      ana_end=ana_end, trade_start=trade_start, trade_end=trade_end)
+    print("3. 进行协整检验")
+    (pairs, pairs_ana_data, pairs_trade_data) = filter_cointegration(ac, ana_date, trade_data)
     # 对所有套利对生成策略图
-    # create_report(ac,pairs,data)
+    print("4. 打印最后的协整分析结果")
+    create_report(pairs, pairs_ana_data, pairs_trade_data)
     print("协整检验完成")
-    return ac, pairs, data
+    return ac, pairs, pairs_ana_data, pairs_trade_data
 
 
 # 适当性品种，这些品种目前无法交易,需要额外申请
 suit_futures = ['IC', 'IF', 'IH', 'T', 'TF', 'TS', 'I', 'SC', 'TA']
 # 目前有问题的合约，粳稻期货目前无人参与交易，一条数据都没有
-error_futures = ['JR']
+# 普麦也无数据
+error_futures = ['JR','PM']
 # 合约代码和交易单位数据，暂时存放在这里
 trade_units = {'AP': 10, 'CF': 5, 'CJ': 5, 'CY': 5, 'FG': 20, 'JR': 20, 'LR': 20, 'MA': 10, 'OI': 10, 'PM': 50,
                'RI': 20, 'RM': 10,
